3d9a87496b15dbd86eacb50b913d9300b570c6fb
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-05-26 19:19:56 +0300

    scroll

--- my-project/src/PracComponent.jsx
+++ my-project/src/PracComponent.jsx
@@ -1,30 +1,61 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
+import "./PracComponent.css";
 
 export default function PracComponent() {
-  //   const [count, setCount] = useState(0);
+  const [scrollProgress, setScrollProgress] = useState(0);
 
-  const [input, setInput] = useState("");
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = window.scrollY;
 
-  const inputRef = useRef();
+      const pageHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
 
-  function ConfirmInput() {
-    console.log(inputRef.current.value);
-    setInput(inputRef.current.value);
-  }
+      const progress = (scrollTop / pageHeight) * 100;
+
+      setScrollProgress(progress);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      {/* <h1>{count}</h1>
+      <button
+        onClick={() =>
+          document
+            .getElementById("section3")
+            .scrollIntoView({ behavior: "smooth" })
+        }
+      >
+        go to section 3
+      </button>
+      <div className="scrollMap">
+        <div
+          className="circle"
+          style={{
+            top: `${scrollProgress}%`,
+          }}
+        />
 
-      <button onClick={() => setCount(count + 1)}>+</button>
+        <div className="columns">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
 
-      <button onClick={() => setCount(count - 1)}>-</button> */}
-
-      <input ref={inputRef} />
-
-      <button onClick={ConfirmInput}>confirm</button>
-
-      <h1>{input}</h1>
+      <section className="section">Section 1</section>
+      <section className="section">Section 2</section>
+      <section id="section3" className="section">
+        Section 3
+      </section>
+      <section className="section">Section 4</section>
     </>
   );
 }
